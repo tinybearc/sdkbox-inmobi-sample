53a0dc1f320e1c950e634555db57b29f6a6ddcd4
hugo <htlxyz@126.com>
2015-12-11 12:06:49 +0800

    fix compile

--- js/frameworks/runtime-src/proj.android/jni/Android.mk
+++ js/frameworks/runtime-src/proj.android/jni/Android.mk
@@ -22,6 +22,6 @@ $(call import-add-path,$(LOCAL_PATH))
 
 
 $(call import-module,scripting/js-bindings/proj.android/prebuilt-mk)
-$(call import-module,tools/simulator/libsimulator/proj.android/prebuilt-mk)
+#$(call import-module,tools/simulator/libsimulator/proj.android/prebuilt-mk)
 $(call import-module, ./sdkbox)
 $(call import-module, ./plugininmobi)
--- lua/frameworks/runtime-src/proj.android/jni/Android.mk
+++ lua/frameworks/runtime-src/proj.android/jni/Android.mk
@@ -25,7 +25,7 @@ include $(BUILD_SHARED_LIBRARY)
 $(call import-add-path,$(LOCAL_PATH))
 
 $(call import-module,scripting/lua-bindings/proj.android/prebuilt-mk)
-$(call import-module,tools/simulator/libsimulator/proj.android/prebuilt-mk)
+#$(call import-module,tools/simulator/libsimulator/proj.android/prebuilt-mk)
 $(call import-module, ./sdkbox)
 $(call import-module, ./plugininmobi)
 
