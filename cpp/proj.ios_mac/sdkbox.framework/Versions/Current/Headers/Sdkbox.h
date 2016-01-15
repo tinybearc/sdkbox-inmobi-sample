7e59e3701aec780d7e9a04a43d24cb25a721d77a
Nite Luo <darkdukey@gmail.com>
2016-01-15 13:51:08 -0800

    upgrade InMobi to 1.5.3.3

--- cpp/proj.ios_mac/sdkbox.framework/Versions/Current/Headers/Sdkbox.h
+++ cpp/proj.ios_mac/sdkbox.framework/Versions/Current/Headers/Sdkbox.h
@@ -7,7 +7,23 @@
 #ifndef _CC_SDKBOX_H_
 #define _CC_SDKBOX_H_
 
-#define SDKBOX_VERSION_STR  "sdkbox V1.5.1.4"
+#define SDKBOX_VERSION_STR  "sdkbox V1.5.3.3"
+
+/**
+ * Only certain compilers support __attribute__((deprecated)).
+ */
+#if defined(__GNUC__) && ((__GNUC__ >= 4) || ((__GNUC__ == 3) && (__GNUC_MINOR__ >= 1)))
+#define SDKBOX_DEPRECATED_ATTRIBUTE __attribute__((deprecated))
+#elif _MSC_VER >= 1400 //vs 2005 or higher
+#define SDKBOX_DEPRECATED_ATTRIBUTE __declspec(deprecated)
+#else
+#define SDKBOX_DEPRECATED_ATTRIBUTE
+#endif
+
+/**
+ * SDKBOX_DEPRECATED(4.0) or SDKBOX_DEPRECATED(4.0, "not going to need this anymore") etc.
+ */
+#define SDKBOX_DEPRECATED(...) SDKBOX_DEPRECATED_ATTRIBUTE
 
 namespace sdkbox {
 
